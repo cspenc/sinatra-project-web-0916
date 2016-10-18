c76339ffcf812d5b98bb35e649ab8124269115cf
calebcox725 <caleb.cox@flatironschool.com>
2016-10-18 17:05:11 -0400

    Just be fantastic all day

--- app/controllers/restaurant_controller.rb
+++ app/controllers/restaurant_controller.rb
@@ -11,12 +11,36 @@ class RestaurantController < ApplicationController
 
   get '/restaurants/:id' do
     @restaurant = Restaurant.find(params[:id])
+    @users = User.all
     erb :"/restaurants/show.html"
   end
 
   post '/restaurants' do
     @restaurant = Restaurant.create(params[:restaurant])
     redirect "/restaurants/#{@restaurant.id}"
+  end
+
+  post '/restaurants/:id' do
+    @restaurant = Restaurant.find(params[:id])
+    UserRestaurant.create(user_id: params[:user][:id], restaurant_id: @restaurant.id)
+    redirect "/restaurants/#{@restaurant.id}"
+  end
+
+  post '/restaurants/:id/edit' do
+    @restaurant = Restaurant.find(params[:id])
+    erb :"/restaurants/edit.html"
+  end
+
+  patch '/restaurants/:id' do
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(params[:restaurant])
+    redirect "/restaurants/#{@restaurant.id}"
+  end
+
+  delete '/restaurants/:id/delete' do
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.delete
+    redirect "/restaurants"
   end
 
 end
