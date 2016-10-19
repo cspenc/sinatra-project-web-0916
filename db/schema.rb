42ce460c91fdf615ebccdcbe999454f9ea9afa63
cspenc <crs992@gmail.com>
2016-10-19 14:04:28 -0400

    added search nearby

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161018193700) do
+ActiveRecord::Schema.define(version: 20161019175356) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20161018193700) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "address"
   end
 
 end
